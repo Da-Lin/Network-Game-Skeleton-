2f9cb229cdb85a077c7f3c42852e1da3d31b6251
Da Lin <dalin@wustl.edu>
2018-11-01 00:09:34 -0500

    initialized basic structure for a multiplayer game

--- Realtime_Database_Test/GameScene.swift
+++ Realtime_Database_Test/GameScene.swift
@@ -8,70 +8,111 @@
 
 import SpriteKit
 import GameplayKit
+import Firebase
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let currentPlayer = 1
+    
+    let rootRef = Database.database().reference()
+    let ballRefX = Database.database().reference().child("ball").child("position").child("x");
+    let ballRefY = Database.database().reference().child("ball").child("position").child("y");
+    let p1Refx = Database.database().reference().child("player1").child("position").child("x");
+    let p1Refy = Database.database().reference().child("player1").child("position").child("y");
+    let p2Refx = Database.database().reference().child("player2").child("position").child("x");
+    let p2Refy = Database.database().reference().child("player2").child("position").child("y");
+    
+    var ball = SKSpriteNode()
+    var player1 = SKSpriteNode()
+    var player2 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        player1 = self.childNode(withName: "player1") as! SKSpriteNode
+        player2 = self.childNode(withName: "player2") as! SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        if currentPlayer == 1{
+            ball.physicsBody?.applyImpulse(CGVector(dx:30, dy:30))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        self.physicsBody = border
+        ballRefX.observe(DataEventType.value) { (snapshot) in
+            let p1PosX = snapshot.value as? String
+            if let posX = p1PosX{
+                if self.currentPlayer != 1{
+                    self.ball.run(SKAction.moveTo(x: CGFloat(truncating: NumberFormatter().number(from: posX)!), duration: 0.002))
+                }
+            }
         }
+        ballRefY.observe(DataEventType.value) { (snapshot) in
+            let p1PosX = snapshot.value as? String
+            if let posX = p1PosX{
+                if self.currentPlayer != 1{
+                    self.ball.run(SKAction.moveTo(y: CGFloat(truncating: NumberFormatter().number(from: posX)!), duration: 0.002))
+                }
+            }
+        }
+        p1Refx.observe(DataEventType.value) { (snapshot) in
+            let p1PosX = snapshot.value as? String
+            if let posX = p1PosX{
+                if self.currentPlayer != 1{
+                    self.player1.run(SKAction.moveTo(x: CGFloat(truncating: NumberFormatter().number(from: posX)!), duration: 0.002))
+                }
+            }
+        }
+        p2Refx.observe(DataEventType.value) { (snapshot) in
+            let p2PosX = snapshot.value as? String
+            if let posX = p2PosX{
+                if self.currentPlayer != 2{
+                    self.player2.run(SKAction.moveTo(x: CGFloat(truncating: NumberFormatter().number(from: posX)!), duration: 0.002))
+                }
+            }
+        }
+
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+       
+        for t in touches {
+            let location = t.location(in: self)
+            if currentPlayer == 1{
+                player1.run(SKAction.moveTo(x: location.x, duration: 0.2))
+                p1Refx.setValue("\(player1.position.x)")
+            }else if currentPlayer == 2{
+                player2.run(SKAction.moveTo(x: location.x, duration: 0.2))
+                p2Refx.setValue("\(player2.position.x)")
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in touches {
+            let location = t.location(in: self)
+            if currentPlayer == 1{
+                player1.run(SKAction.moveTo(x: location.x, duration: 0.2))
+                p1Refx.setValue("\(player1.position.x)")
+            }else if currentPlayer == 2{
+                player2.run(SKAction.moveTo(x: location.x, duration: 0.2))
+                p2Refx.setValue("\(player2.position.x)")
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -85,5 +126,9 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if(currentPlayer == 1){
+            ballRefX.setValue("\(ball.position.x)")
+            ballRefY.setValue("\(ball.position.y)")
+        }
     }
 }
--- Realtime_Database_Test/GameViewController.swift
+++ Realtime_Database_Test/GameViewController.swift
@@ -15,6 +15,8 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("ios")
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
